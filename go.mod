98ac994aec5c04b1cd9917bfbf6d0453b0860465
cavatgb1737 <cavatgb@gmail.com>
2025-11-15 21:09:55 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cavatz/marble-estimation
+
+go 1.25.2
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	for {
+		const g float64 = 9.81
+		const P float64 = 1000.0
+		const METERS_CONVERSION float64 = 0.0254
+		const MARBLE_WEIGHT int = 4
+		const p int = 1240
+
+		scanner := bufio.NewScanner(os.Stdin)
+
+		fmt.Printf("volume of filled in boat or x to stop: ")
+		scanner.Scan()
+
+		Vtext := scanner.Text()
+		if Vtext == "x" {
+			break
+		}
+
+		V, err := strconv.ParseFloat(Vtext, 64)
+		handleError(err)
+
+		V *= math.Pow(METERS_CONVERSION, 3)
+
+		grams := (V * P) * 1000
+
+		fmt.Printf("volume of hollowed boat: ")
+		scanner.Scan()
+
+		v, err := strconv.ParseFloat(scanner.Text(), 64)
+		handleError(err)
+
+		v *= math.Pow(METERS_CONVERSION, 3)
+
+		m := (float64(p) * v) * 1000
+
+		marbles_held := (grams - m) / float64(MARBLE_WEIGHT)
+		fmt.Printf("%v marbles held before sinking. \n", marbles_held)
+	}
+	//F = (P * V) * g
+
+}
+
+func handleError(err error) {
+	if err != nil {
+		fmt.Printf("Error parsing volume of the boat.")
+		os.Exit(1)
+	}
+}
